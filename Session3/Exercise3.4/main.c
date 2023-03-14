82b99c7603fbf2843f781a778d97d28c39473223
IB-315168 <315168@via.dk>
2023-03-14 10:35:11 +0100

    Session 3 ex4

--- Session3/Exercise3.4/main.c
+++ Session3/Exercise3.4/main.c
@@ -3,9 +3,51 @@
 
 int main() {
     char text[20];
+
+    char str1[20];
+    char str2[20];
+
+    char dest[20];
+    char src[20];
+
+    char dup[20];
+    char* pDup;
+
     while(1 == 1) {
-        scanf("%20s", &text);
-        printf("%d\n", my_strlen(text));
+        // printf("-----------Length-----------\n");
+        // printf("Type in word:\n");
+        // scanf("%20s", &text);        
+        // printf("Length: %d\n", my_strlen(&text));
+        // printf("----------------------------\n\n");
+
+        // printf("---------Comparison---------\n");
+        // printf("Type in first word:\n");
+        // scanf("%20s", &str1);
+        // printf("Type in second word:\n");
+        // scanf("%20s", &str2);
+        // printf("Result: %d\n", my_strcmp(&str1, &str2));
+        // printf("----------------------------\n\n");
+
+        // printf("------------Copy------------\n");
+        // printf("Type the word to be copied:\n");
+        // scanf("%20s", &src);
+        // my_strcpy(&dest, &src);
+        // printf("Result: \n");
+        // printf("Source: [%p]: %20s\n", &src, src);
+        // printf("Destination: [%p]: %20s\n", &dest, dest);
+        // printf("----------------------------\n\n");
+
+        printf("----------Duplicate---------\n");
+        printf("Type the word to be duplicated:\n");
+        scanf("%20s", &dup);
+
+        pDup = my_strdup(dup);
+
+        printf("Result: \n");
+        printf("Source: [%p]: %20s\n", &dup, dup);
+        printf("Destination: [%p]: %20s\n", &pDup, *pDup);
+        printf("----------------------------\n\n");
     }
+
     return 0;
 }
--- Session3/Exercise3.4/mystr.c
+++ Session3/Exercise3.4/mystr.c
@@ -2,5 +2,45 @@
 #include "mystr.h"
 
 int my_strlen(const char* str) {
+    int i = 0;
+    while(str[i] != '\0') {
+        i += 1;
+    }
+
+    return i;
+}
+
+int my_strcmp(const char* str1, const char* str2) {
+    int i = 0;
+    
+    while(str1[i] != '\0' && str2[i] != '\0') {
+        if(str1[i] == str2[i]) {
+            i++;
+        } else {
+            return -(str1[i] < str2[i]) + (str1[i] > str2[i]);
+        }
+    }
+
+    if(str1[i] == '\0' && str1[i] != str2[i]) {
+        return -(str1[i] < str2[i]) + (str1[i] > str2[i]);
+    }
+
     return 0;
+}
+
+char* my_strcpy(char* dest, const char* src) {
+    int i = 0;
+
+    while(src[i] != '\0') {
+        dest[i] = src[i];
+        i++;
+    }
+
+    dest[i++] = '\0';
+    return dest;
+}
+
+char* my_strdup(const char* str) {
+    char t[sizeof(str)];
+    return my_strcpy(t, &str);
 }
